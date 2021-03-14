dfeb57b725637afe2a95100515ea00e0ae841187
radharukminiY <rukku558@gmail.com>
2021-03-14 22:08:51 +0530

    commit 4

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ var port = 9600;
 var hotelRouter = require('./src/router/hotelRouter');
 var cityRouter =require('./src/router/cityRouter');
 
+app.use(express.static(__dirname+'/public'))
+app.set('views','./views');
+app.set('view  engine','ejs');
+
 app.get('/',(req,res) =>{
-  res.send('Node js ')
+  res.render('index.ejs')
 });
 
 app.use('/hotel',hotelRouter)
